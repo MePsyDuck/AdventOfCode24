b148e200fcdfb3e06c203ef5399a5dcca3474eff
Sumeet Swain <sumit231996@gmail.com>
2024-12-11 10:03:52 +0530

    feat: Add 2024 day 10

---
+++ day10/part1.py
@@ -0,0 +1,39 @@
+def main():
+    grid = []
+    with open('input.txt') as f:
+        for line in f.readlines():
+            grid.append([int(c) if c != '.' else -1 for c in line.strip()])
+
+    s = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            s += trail_score(grid, i, j, 0, set())
+    print(s)
+
+
+def trail_score(grid, i, j, h, nines):
+    if 0 <= i < len(grid) and 0 <= j < len(grid[0]):
+        if h == 9 == grid[i][j] and (i, j) not in nines:
+            nines.add((i, j))
+            return 1
+        elif grid[i][j] == h:
+            return (trail_score(grid, i + 1, j, h + 1, nines) +
+                    trail_score(grid, i - 1, j, h + 1, nines) +
+                    trail_score(grid, i, j + 1, h + 1, nines) +
+                    trail_score(grid, i, j - 1, h + 1, nines))
+        else:
+            return 0
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    import time
+
+    start = time.perf_counter()
+    main()
+    end = time.perf_counter()
+    if end - start < 1:
+        print(f"{(end - start) * 1000:.2f} milliseconds")
+    else:
+        print(f"{end - start:.6f} seconds")
